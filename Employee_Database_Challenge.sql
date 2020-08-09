06befe605e670301f44ff81031cee1f585ab307e
Tavender22 <66438404+Tavender22@users.noreply.github.com>
2020-08-09 16:35:11 -0700

    Updating VS Code

--- Employee_Database_Challenge.sql
+++ Employee_Database_Challenge.sql
@@ -12,6 +12,8 @@ LEFT JOIN titles AS t
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
+SELECT * FROM retirement_titles
+
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 	rt.first_name,
@@ -28,7 +30,9 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY count DESC, ut.title;
-	
+
+SELECT * FROM retiring_titles;
+
 -- Mentorship eligibilty listing
 SELECT DISTINCT ON (e.emp_no) e.emp_no,
 	e.first_name,
@@ -47,3 +51,42 @@ WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND de.to_date = '9999-01-01'
 ORDER BY e.emp_no;
 
+SELECT * FROM mentorship_eligibility;
+
+-- list of title mentorship
+SELECT COUNT(me.emp_no), 
+	me.title
+INTO mentorship_titles
+FROM mentorship_eligibility as me
+GROUP BY me.title
+ORDER BY count DESC, me.title;
+
+SELECT * FROM retiring_titles;
+
+-- number of employees with less than 10 years at PH.
+SELECT * FROM employees
+ORDER BY hire_date DESC
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO mentor_candidates
+FROM employees AS e
+LEFT JOIN titles AS t
+	ON e.emp_no = t.emp_no
+WHERE (e.hire_date BETWEEN '1990-01-31' AND '9999-01-01') 
+AND (t.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+-- list of title mentorship count
+SELECT COUNT(mc.emp_no), 
+	mc.title
+INTO candidate_count
+FROM mentor_candidates as mc
+GROUP BY mc.title
+ORDER BY count DESC, mc.title;
+
+SELECT * FROM candidate_count
